dcbc814bfbfc1758c92002a55351d740ebd9df87
kanosaki <kanosaki@gmail.com>
2018-11-23 00:32:30 +0900

    Change tracer strategy. Remove timeStack

--- src/main/kotlin/epimetheus/engine/Tracer.kt
+++ src/main/kotlin/epimetheus/engine/Tracer.kt
@@ -19,27 +19,19 @@ interface Tracer {
     }
 }
 
-data class EvalLog(val ast: Expression, val elapsedNs: Long, val result: Value, val depth: Int)
+data class EvalLog(val ast: Expression, val timestamp: Long, val result: Value, val depth: Int)
 
 class FullLoggingTracer : Tracer {
-    private val timerStack = Stack<Long>()
-    private var lastDepth = -1
-
+    private var beginTime: Long? = null
     val log = LinkedList<EvalLog>()
 
     override fun enteringEvalExpr(ast: Expression, depth: Int) {
-        // step in
-        if (depth >= lastDepth) {
-            timerStack.push(System.nanoTime())
-        }
-        lastDepth = depth
+        beginTime = System.nanoTime()
     }
 
     override fun onEvalExpr(ast: Expression, result: Value, depth: Int) {
-        val finished = System.nanoTime()
         try {
-            val beginTime = timerStack.pop()
-            log.add(EvalLog(ast, finished - beginTime, result, timerStack.size))
+            log.add(EvalLog(ast, System.nanoTime(), result, depth))
         } catch (e: EmptyStackException) {
             println("EVAL LOG")
             println(log.joinToString("\n"))
@@ -48,6 +40,10 @@ class FullLoggingTracer : Tracer {
     }
 
     fun printTrace(out: PrintStream = System.out, indent: Int = 2): String {
+        if (log.isEmpty()) {
+            return "EMPTY LOG"
+        }
+        val bt = beginTime!!
         val sb = StringBuilder()
         for (entry in log.asReversed()) {
             if (entry.depth != 0) {
@@ -61,7 +57,7 @@ class FullLoggingTracer : Tracer {
                 out.print('|')
                 out.print(" ".repeat(indent * entry.depth - 1))
             }
-            out.println("--> ${entry.elapsedNs / 1000}us ${entry.result}")
+            out.println("--> ${(entry.timestamp - bt) / 1000}us ${entry.result}")
         }
 
         return sb.toString()
